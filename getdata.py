abec3d904a6a0fe4eb2235a4366aacb1af21a541
Mohindran S.R <mohindrans.r@Driftkings-MacBook-Air.local>
2022-10-26 09:21:50 +0530

    prod

--- getdata.py
+++ getdata.py
@@ -1,9 +1,9 @@
 from app import *
 import psycopg2
 def get_db_connection():
-    conn = psycopg2.connect(host='localhost',
-                            database='minor',
-                            user='postgres',
-                            password='password')
+    conn = psycopg2.connect(host='ec2-3-213-66-35.compute-1.amazonaws.com',
+                            database='dbjb1b02k0uu4c',
+                            user='ndoxopmrmkwvtm',
+                            password='f08c1327a23c31e9e55f28308b018255832b41f879efbf5a69e139b89786267a')
     return conn
 
