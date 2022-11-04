9108bfca76afabda8b41cee7d9da324fe1822dbd
Mohindran S.R <s.r.mohindran@gmail.com>
2022-11-04 14:43:03 +0530

    nn

---
+++ constants.py
@@ -0,0 +1,4 @@
+hosts='ec2-3-213-66-35.compute-1.amazonaws.com'
+databases='dbjb1b02k0uu4c'
+users='ndoxopmrmkwvtm'
+passwords='f08c1327a23c31e9e55f28308b018255832b41f879efbf5a69e139b89786267a'
--- getdata.py
+++ getdata.py
@@ -1,9 +1,11 @@
 from app import *
+from constants import *
 import psycopg2
 def get_db_connection():
-    conn = psycopg2.connect(host='ec2-3-213-66-35.compute-1.amazonaws.com',
-                            database='dbjb1b02k0uu4c',
-                            user='ndoxopmrmkwvtm',
-                            password='f08c1327a23c31e9e55f28308b018255832b41f879efbf5a69e139b89786267a')
+    
+    conn = psycopg2.connect(host=hosts,
+                            database=databases,
+                            user=users,
+                            password=passwords)
     return conn
 
